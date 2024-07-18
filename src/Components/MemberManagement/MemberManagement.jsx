65a737f8586b2529a9fee07839614d8e6f3388df
Bajjuri Sumanth <bajjuri.sumanth@comviva.com>
2024-07-18 23:27:42 +0530

    making code cleaner 02

--- src/Components/MemberManagement/MemberManagement.jsx
+++ src/Components/MemberManagement/MemberManagement.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import './MemberManagement.css';
-import { AiFillEdit, AiFillDelete } from 'react-icons/ai'; // Updated import
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai'; 
 
 const supabaseUrl = 'https://vfcdcttmvwskzqfpuqxq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmY2RjdHRtdndza3pxZnB1cXhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4NjIwMDUsImV4cCI6MjAzNTQzODAwNX0.Y94iLW2Piy7Cqdcu9S0I9k_rr3CD99RbE--MDtpI4P0';
